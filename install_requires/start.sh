43df56e162947dfa9b8dfd6b12520bf4dfa311d5
Syahrul Irwansyah <ach.syahrul_99@gmail.com>
2023-06-19 13:51:01 +0700

    Add network.sh and dep.sh

--- install_requires/start.sh
+++ install_requires/start.sh
@@ -23,32 +23,6 @@ cd /home/$USER
 git clone https://github.com/ROBOTIS-GIT/robotis_tools.git
 
 
-echo "[Autoconnect Wifi]"
-sh -c "sudo echo \"network:\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"    ethernets:\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"        eth0:\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"            dhcp4: true\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"            optional: true\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"    version: 2\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"    wifis:\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"        rendered: networkd\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"        wlan0:\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"            dhcp4: true\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"            optional: true\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"            access-points:\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"                \"Irbot_slam:\"\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"                    password: \"1234567890\"\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"        wlan0:\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"            dhcp4: true\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"            optional: true\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"            access-points:\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"                \"River:\"\" >> /etc/netplan/50-cloud-init.yaml"
-sh -c "sudo echo \"                    password: \"tothemoooon\"\" >> /etc/netplan/50-cloud-init.yaml"
-
-sudo netplan generate
-sudo netplan apply
-
-
 echo "[Join ZeroTier Network]"
 curl -s https://install.zerotier.com | sudo bash
 sudo zerotier-cli join e5cd7a9e1cfb25b9
