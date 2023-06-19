a60eaf545e681efae7da1fd25bc0e7b9d7fca266
Syahrul Irwansyah <ach.syahrul_99@gmail.com>
2023-06-19 14:04:43 +0700

    Add rosserial-install

--- install_requires/dep.sh
+++ install_requires/dep.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "[Install ROS Dependencies]"
+sudo apt-get install -y ros-melodic-rosserial-arduino
+sudo apt-get install -y ros-melodic-rosserial
 sudo apt-get install -y ros-melodic-rplidar-ros
 sudo apt-get install -y ros-melodic-slam-karto ros-melodic-hector-slam ros-melodic-gmapping
 sudo apt-get install -y ros-melodic-move-base
